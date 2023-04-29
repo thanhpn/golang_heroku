762f68625fc0b7d2fc3ed61b3527b798876eb456
Thành Phạm Ngọc <ngocthanh2207@gmail.com>
2023-04-29 15:52:41 +0700

    fix cors

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	v1 "github.com/ydhnwb/golang_heroku/handler/v1"
 )
@@ -22,6 +25,18 @@ var (
 func main() {
 	// defer config.CloseDatabaseConnection(db)
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "OPTIONS"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	// authRoutes := server.Group("api/auth")
 	// {
